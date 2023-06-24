9a504e9ada13bb2bad57498be87f5b28fb2d68ed
sunnyrithvik <sribhashyamrithvik@gmail.com>
2023-06-24 16:09:05 +0530

    initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("june 20 2023");
+  date.setDate(date.getDate() + count);
+
+  function incCount() {
+    setCount((s) => s + step);
+  }
+  function decCount() {
+    setCount((s) => s - step);
+  }
+  function incStep() {
+    setStep((p) => p + 1);
+  }
+  function decStep() {
+    setStep((p) => p - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <span>
+          <button onClick={decStep}>-</button>
+          <span>Step: {step}</span>
+          <button onClick={incStep}>+</button>
+        </span>
+      </div>
+      <div>
+        <span>
+          <button onClick={decCount}>-</button>
+          <span>Count: {count}</span>
+          <button onClick={incCount}>+</button>
+        </span>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} day from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </div>
   );
 }
